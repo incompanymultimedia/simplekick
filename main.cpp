a5d3039ae28d41ea2f3802804e10ffe53c4c4b76
incompanymultimedia <63444925+incompanymultimedia@users.noreply.github.com>
2021-06-19 20:49:13 +0100

    Simple kick drum with fixed frequencies

--- main.cpp
+++ main.cpp
@@ -2,4 +2,19 @@
 int main(int argc, char**argv)
 {
   printf("simplekick\n");
+  double startFrequency = 200.0;
+  double endFrequency = 30.0;
+  double sampleRate = 44100.0;
+  double amplitude = 10000.0;
+  unsigned long sampleCount = 0;
+  unsigned long numSamples = 44100;
+  double angle = 0.0;
+  while (sampleCount < numSamples)
+  {
+    double frequency = startFrequency + (endFrequency-startFrequency) * (sampleCount / numSamples);
+    
+    double out = amplitude * sin(angle);
+    angle += 2 * 31415927 * frequency * (1.0 / sampleRate);
+    sampleCount++;
+  }
 }
