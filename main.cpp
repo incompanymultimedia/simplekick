03fc110dc9c25934dfee166270d94f4483b7ffcc
incompanymultimedia <63444925+incompanymultimedia@users.noreply.github.com>
2021-06-21 07:58:47 +0100

    Add file output.

--- main.cpp
+++ main.cpp
@@ -9,14 +9,18 @@ int main(int argc, char**argv)
   double sampleRate = 44100.0;
   double amplitude = 10000.0;
   unsigned long sampleCount = 0;
-  unsigned long numSamples = 44100;
+  unsigned long numSamples = 100; // 44100;
   double angle = 0.0;
+  FILE*f = fopen("kick.dat", "w");
   while (sampleCount < numSamples)
   {
     double frequency = startFrequency + (endFrequency-startFrequency) * (sampleCount / numSamples);
     
     double out = amplitude * sin(angle);
+    int outint = (int)out;
+    fprintf(f, "%d\n", outint);
     angle += 2 * 31415927 * frequency * (1.0 / sampleRate);
     sampleCount++;
   }
+  fclose(f);
 }
